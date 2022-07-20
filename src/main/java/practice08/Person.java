e2c15d983c42f486033a87f18795d9a1b9fe3da8
blank1024 <13380497335@163.com>
2022-07-20 20:51:43 +0800

    feat: implement practice08 to pass test

---
+++ src/main/java/practice08/Person.java
@@ -0,0 +1,48 @@
+package practice08;
+
+import java.util.Objects;
+
+public class Person {
+    private int id;
+    private String name;
+    private int age;
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public Person(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public String introduce() {
+        return "My name is " + name + ". I am " + age + " years old.";
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Person person = (Person) o;
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
---
+++ src/main/java/practice08/Student.java
@@ -0,0 +1,45 @@
+package practice08;
+
+import practice08.Klass;
+
+public class Student extends Person{
+    private int klassNum;
+    private Klass klass;
+
+    public Student(int id, String name, int age, int klassNum) {
+        super(id, name, age);
+        this.klassNum = klassNum;
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public int getKlassNum() {
+        return klassNum;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlassNum(int klassNum) {
+        this.klassNum = klassNum;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public String introduce() {
+        String introduce = "";
+        if (klass.getLeader() != null) {
+            introduce = " Leader of Class " + klass.getNumber() + ".";
+        } else {
+            introduce = " at Class " + klass.getNumber() + ".";
+        }
+        return "My name is " + super.getName() + ". I am " + super.getAge() +
+                " years old. I am a Student. I am" + introduce;
+    }
+}
